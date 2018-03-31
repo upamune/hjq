d067d31bc4cdcb4c30cdafaa6704a417f5081fd6
upamune <info@serizawa.me>
2018-04-01 00:36:13 +0900

    hjq関数を実装した

--- src/Data/Hjq.hs
+++ src/Data/Hjq.hs
@@ -1,6 +1,18 @@
-module Data.Hjq where
+{-# LANGUAGE OverloadedStrings #-}
 
+module Data.Hjq (hjq) where
+
+import Control.Error.Util
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
+import Data.ByteString.Lazy
+import Data.Hjq.Parser
+import Data.Hjq.Query
 import Data.Text
 
-import Data.Hjq.Parser
+hjq :: ByteString -> Text -> Either Text ByteString
+hjq jsonString queryString = do
+    value <- note "Invalid json format." $ decode jsonString
+    query <- parseJqQuery queryString
+    executeQuery query value >>= return . encodePretty
 
