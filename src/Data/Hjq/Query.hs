cd6b7e955658211ff172dc492c438c268690083e
upamune <info@serizawa.me>
2018-03-31 18:45:52 +0900

    フィルタの実行関数を書いた

--- src/Data/Hjq/Query.hs
+++ src/Data/Hjq/Query.hs
@@ -1,2 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.Hjq.Query where
 
+import Control.Applicative
+import Control.Monad (join)
+import Control.Lens.Operators ((^?))
+import Data.Aeson
+import Data.Aeson.Lens
+import Data.Attoparsec.Text
+import Data.Hjq.Parser
+import Data.Semigroup ((<>))
+import Data.Text
+
+import qualified Data.HashMap.Strict as H
+import qualified Data.Vector as V
+
+applyFilter :: JqFilter -> Value -> Either Text Value
+applyFilter (JqField fieldName n) obj@(Object _) = join $ noteNotFoundError fieldName (fmap (applyFilter n) (obj ^? key fieldName))
+applyFilter (JqIndex index n) array@(Array _) = join $ noteOutOfRangeError index (fmap (applyFilter n) (array ^? nth index))
+applyFilter JqNil v = Right v
+applyFilter f o = Left $ "unexpected pattern : " <> tshow f <> ":" <> tshow o
+
+noteNotFoundError :: Text -> Maybe a -> Either Text a
+noteNotFoundError _ (Just x) = Right x
+noteNotFoundError s Nothing = Left $ "field name not found" <> s
+
+noteOutOfRangeError :: Int -> Maybe a -> Either Text a
+noteOutOfRangeError _ (Just x) = Right x
+noteOutOfRangeError s Nothing = Left $ "out of range: " <> tshow s
+
+tshow :: Show a => a -> Text
+tshow = pack . show
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Test.HUnit
-import Data.Hjq
+import Control.Lens ((^?))
+import Data.Aeson
+import Data.Aeson.Lens
 import Data.Hjq.Parser
+import Data.Hjq.Query
+import Data.Text
+import Test.HUnit
+
+import qualified Data.HashMap.Strict as H
+import qualified Data.Vector as V
 
 main :: IO ()
 main = do
@@ -10,7 +17,8 @@ main = do
         [ jqFilterParserTest
         , jqFilterParserSpaceTest
         , jqQueryParserTest
-        , jqQueryParserSpaceTest ]
+        , jqQueryParserSpaceTest
+        , applyFilterTest ]
     return ()
 
 jqFilterParserTest :: Test
@@ -44,3 +52,36 @@ jqQueryParserSpaceTest = TestList
     , "jqQueryParser test 2" ~: parseJqQuery "[ .hoge , .piyo ]" ~?= Right (JqQueryArray [JqQueryFilter (JqField "hoge" JqNil), JqQueryFilter (JqField "piyo" JqNil)])
     , "jqQueryParser test 3" ~: parseJqQuery "{ \"hoge\" : [ ] , \"piyo\" : [ ] }" ~?= Right (JqQueryObject [("hoge", JqQueryArray []), ("piyo", JqQueryArray [])])
     ]
+
+testData :: Value
+testData = Object $ H.fromList
+    [ ("string-field", String "string value")
+    , ("nested-field", Object $ H.fromList
+        [ ("inner-string", String "inner value")
+        , ("inner-number", Number 100)
+        ]
+    )
+    , ("array-field", Array $ V.fromList
+        [ String "first field"
+        , String "next field"
+        , Object (H.fromList
+            [("object-in-array", String "string value in object-in-array")])
+        ]
+        )
+    ]
+
+applyFilterTest :: Test
+applyFilterTest = TestList
+    [ "applyFilter test 1" ~: applyFilter (unsafeParseFilter ".") testData ~?= Right testData
+    , "applyFilter test 2" ~: (Just $ applyFilter (unsafeParseFilter ".string-field") testData) ~?= fmap Right (testData ^? key "string-field")
+    , "applyFilter test 3" ~: (Just $ applyFilter (unsafeParseFilter ".nested-field.inner-string") testData) ~?= fmap Right (testData ^? key "nested-field" . key "inner-string")
+    , "applyFilter test 4" ~: (Just $ applyFilter (unsafeParseFilter ".nested-field.inner-number") testData) ~?= fmap Right (testData ^? key "nested-field" . key "inner-number")
+    , "applyFilter test 5" ~: (Just $ applyFilter (unsafeParseFilter ".array-field[0]") testData) ~?= fmap Right (testData ^? key "array-field" . nth 0)
+    , "applyFilter test 6" ~: (Just $ applyFilter (unsafeParseFilter ".array-field[1]") testData) ~?= fmap Right (testData ^? key "array-field" . nth 1)
+    , "applyFilter test 7" ~: (Just $ applyFilter (unsafeParseFilter ".array-field[2].object-in-array") testData) ~?= fmap Right (testData ^? key "array-field" . nth 2 . key "object-in-array")
+    ]
+
+unsafeParseFilter :: Text -> JqFilter
+unsafeParseFilter t = case parseJqFilter t of
+    Right f -> f
+    Left s -> error $ "PARSE FAILURE IN A TEST: " ++ unpack s
